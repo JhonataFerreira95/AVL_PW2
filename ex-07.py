1e7b9948693df025dcc77049f0ca1ec0ae3ce00e
MauricioIF <166017257+MauricioIF@users.noreply.github.com>
2024-07-05 15:01:35 -0300

    Delete ex-07.py

--- ex-07.py
+++
@@ -1,9 +0,0 @@
-import math
-palpite = int(input('Escreva um numero de 1 a 10.000 para verificarmos se ele é primo '))
-raiz = math.sqrt(palpite)
-
-if palpite == 2 or palpite == 3:
-    print('é um numero primo')
-
-elif palpite % 2 == 0:
-    print('nao é primo')
